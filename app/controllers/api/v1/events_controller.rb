e30a4949fd34044e1443b0a6ba6aae9486119f79
dcarrano <dcarrano@gmail.com>
2016-06-19 19:11:33 -0400

    working on basic new and delete actions

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,13 +2,14 @@ module Api
   module V1
     class EventsController < ApplicationController
 
+      before_action :find_event, only: [:show, :delete]
+
       def index
         render json: current_user.events.order('date').includes(:gifts, :recipients), include: ['gifts'], include: ['recipients']
       end
 
       def show
-        event = current_user.events.find(params[:id])
-        render json: event
+        render json: @event
       end
 
       def create
@@ -18,10 +19,18 @@ module Api
         render json: event
       end
 
+      def destroy
+        @event.destroy
+      end
+
       private
 
       def event_params
         params.require(:data).require(:attributes).permit(:title, :description, :date)
+      end
+
+      def find_event
+        @event = current_user.events.find(params[:id])
       end
     end
   end
--- app/controllers/api/v1/gifts_controller.rb
+++ app/controllers/api/v1/gifts_controller.rb
@@ -2,19 +2,36 @@ module Api
   module V1
     class GiftsController < ApplicationController
 
+      before_action :find_gift, only: [:show, :delete]
+
       def index
         render json: user_gifts
       end
 
       def show
-        gift = user_gifts.find(params[:id])
-        render json: gift
+        render json: @gift
+      end
+
+      def create
+        Gift.create(create_params)
+      end
+
+      def destroy
+        @gift.destroy
       end
 
       private
 
       def user_gifts
         Gift.joins('INNER JOIN events ON gifts.event_id=events.id INNER JOIN users ON events.user_id = users.id').where('users.id = ?', current_user.id).order('gifts.priority')
+      end
+
+      def find_gift
+        @gift = user_gifts.find(params[:id])
+      end
+
+      def create_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:name, :url, :price, :priority])
       end
     end
   end
