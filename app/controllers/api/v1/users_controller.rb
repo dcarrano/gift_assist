01724404c114bc4144744bf926f0b5be258579cb
dcarrano <dcarrano@gmail.com>
2016-06-19 15:53:28 -0400

    create user functionality added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class UsersController < ApplicationController
 
+    skip_before_action :authenticate, only: [:create]
+
       def index
         render json: User.includes(:recipients, :events), include: ['recipients'], include: ['events']
       end
@@ -11,6 +13,15 @@ module Api
         render json: current_user
       end
 
+      def create
+        User.create(create_params)
+      end
+
+      private
+
+      def create_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:email, :password, :password_confirmation])
+      end
     end
   end
 end
